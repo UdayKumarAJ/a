a0166ed699af46d666c9db09889a131acbb4caad
UdayKumarAJ <udaykumarajgmail.com>
2021-02-16 14:27:41 +0530

    temp

--- src/test/java/com/swagen/qaswb/testcase/ui/LoginTest.java
+++ src/test/java/com/swagen/qaswb/testcase/ui/LoginTest.java
@@ -1,21 +1,26 @@
 package com.swagen.qaswb.testcase.ui;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
 	synchronized public static void main(String[] args) throws InterruptedException {
-		Thread t = new Thread();
-		t.run();
 		
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/webDriverBinaries/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
 		String title = driver.getTitle();
 		System.out.println(title);
-		 
-		driver.wait(5000);
-		driver.quit();
-		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		//driver.findElement(By.className(""));
+//		driver.findElement(By.cssSelector("text")).sendKeys("_Uday_Kumar_A_J");
+//		driver.findElement(By.name("password")).sendKeys("uday9986466152");
+//		driver.findElement(By.id("submit")).click();
+		driver.findElement(By.id("email")).sendKeys("udaykumaraj@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("12345");
+		driver.findElement(By.id("u_0_b")).click();
+	
 	}
 }
